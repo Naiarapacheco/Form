66a398715dd3817f8d8c8364ea93f5ed10604d86
Naiara Pacheco <naiarapachecosantos@gmail.com>
2024-08-06 12:47:31 -0300

    ReactiveFormsModule add

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
+
+  forms = new FormGroup({
+    name: new FormControl(''),
+    age: new FormControl(null),
+    city: new FormControl('')
+  })
 
 }
